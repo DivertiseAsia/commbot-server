f784a337e12db4bb4436452faa39e91d2d028412
Hunter Williams <hgw2es@virginia.edu>
2023-06-09 16:07:09 +0700

    move card message into a helper

---
+++ cards-server-app/comm_manager/helpers.py
@@ -0,0 +1,89 @@
+def flex_json_card_image_with_price(image, price):
+    return {
+        "type": "bubble",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "image",
+                    "url": image,
+                    "size": "full",
+                    "aspectMode": "cover",
+                    "aspectRatio": "7:10",
+                    "gravity": "center",
+                },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "position": "absolute",
+                    "background": {
+                        "type": "linearGradient",
+                        "angle": "0deg",
+                        "endColor": "#00000000",
+                        "startColor": "#00000099",
+                    },
+                    "width": "100%",
+                    "height": "40%",
+                    "offsetBottom": "0px",
+                    "offsetStart": "0px",
+                    "offsetEnd": "0px",
+                },
+                {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "Pricing",
+                                            "size": "lg",
+                                            "color": "#ffffff",
+                                            "align": "end",
+                                        }
+                                    ],
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": f"${price}",
+                                            "color": "#ffffff",
+                                            "size": "md",
+                                            "align": "end",
+                                        }
+                                    ],
+                                },
+                            ],
+                            "spacing": "xs",
+                        }
+                    ],
+                    "position": "absolute",
+                    "offsetBottom": "50%",
+                    "offsetStart": "50%",
+                    "offsetEnd": "0px",
+                    "paddingAll": "15px",
+                    "paddingEnd": "22px",
+                    "background": {
+                        "type": "linearGradient",
+                        "startColor": "#99999900",
+                        "endColor": "#999999AA",
+                        "centerColor": "#99999955",
+                        "angle": "90deg",
+                    },
+                    "width": "50%",
+                },
+            ],
+            "paddingAll": "0px",
+        },
+    }
--- cards-server-app/comm_manager/views.py
+++ cards-server-app/comm_manager/views.py
@@ -58,6 +58,7 @@ def handle_message(event):
             scryfall_card_image,
             scryfall_card_price,
         )
+        from helpers import flex_json_card_image_with_price
 
         cards = scryfall_search(event.message.text)
         card = scryfall_first_card(cards)
@@ -74,94 +75,3 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text=event.message.text)
         )
-
-
-def flex_json_card_image_with_price(image, price):
-    return {
-        "type": "bubble",
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "image",
-                    "url": image,
-                    "size": "full",
-                    "aspectMode": "cover",
-                    "aspectRatio": "7:10",
-                    "gravity": "center",
-                },
-                {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [],
-                    "position": "absolute",
-                    "background": {
-                        "type": "linearGradient",
-                        "angle": "0deg",
-                        "endColor": "#00000000",
-                        "startColor": "#00000099",
-                    },
-                    "width": "100%",
-                    "height": "40%",
-                    "offsetBottom": "0px",
-                    "offsetStart": "0px",
-                    "offsetEnd": "0px",
-                },
-                {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "contents": [
-                        {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                                {
-                                    "type": "box",
-                                    "layout": "horizontal",
-                                    "contents": [
-                                        {
-                                            "type": "text",
-                                            "text": "Pricing",
-                                            "size": "lg",
-                                            "color": "#ffffff",
-                                            "align": "end",
-                                        }
-                                    ],
-                                },
-                                {
-                                    "type": "box",
-                                    "layout": "horizontal",
-                                    "contents": [
-                                        {
-                                            "type": "text",
-                                            "text": f"${price}",
-                                            "color": "#ffffff",
-                                            "size": "md",
-                                            "align": "end",
-                                        }
-                                    ],
-                                },
-                            ],
-                            "spacing": "xs",
-                        }
-                    ],
-                    "position": "absolute",
-                    "offsetBottom": "50%",
-                    "offsetStart": "50%",
-                    "offsetEnd": "0px",
-                    "paddingAll": "15px",
-                    "paddingEnd": "22px",
-                    "background": {
-                        "type": "linearGradient",
-                        "startColor": "#99999900",
-                        "endColor": "#999999AA",
-                        "centerColor": "#99999955",
-                        "angle": "90deg",
-                    },
-                    "width": "50%",
-                },
-            ],
-            "paddingAll": "0px",
-        },
-    }
