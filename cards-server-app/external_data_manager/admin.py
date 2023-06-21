e5b766acf98f5a37fc7118a83d35fd436d82419c
Hunter Williams <hgw2es@virginia.edu>
2023-06-21 23:21:01 +0700

    allow refresh card via dropdown

--- cards-server-app/external_data_manager/admin.py
+++ cards-server-app/external_data_manager/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
 from external_data_manager.models import MtgCard
+from .helpers import update_query_from_scryfall
 
 
 class MtgCardAdminView(ImportExportModelAdmin):
+    actions = ["refresh_data"]
     model = MtgCard
     list_display = (
         "name",
@@ -16,6 +18,10 @@ class MtgCardAdminView(ImportExportModelAdmin):
     )
 
     search_fields = ("id", "name", "search_text", "external_id")
+
+    def refresh_data(self, request, queryset):
+        for obj in queryset:
+            update_query_from_scryfall(obj.search_text)
 
 
 admin.site.register(MtgCard, MtgCardAdminView)
--- cards-server-app/external_data_manager/helpers.py
+++ cards-server-app/external_data_manager/helpers.py
@@ -29,6 +29,51 @@ def get_ckd_price_and_img(card: str):
         return ("", "")
 
 
+def update_query_from_scryfall(query):
+    url = "https://api.scryfall.com/cards/search"
+    params = {
+        "q": query,
+        "unique": "prints",
+        "order": "name",
+        "dir": "asc",
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        if data["object"] == "error":
+            print("Scryfall had an error", data)
+            return None
+
+        if data["total_cards"] > 0:
+            card = data["data"][0]
+
+            card_obj, _ = MtgCard.objects.get_or_create(search_text=query)
+            card_obj.name = card["name"]
+            if "image_uris" in card:
+                card_obj.image_url = card["image_uris"]["normal"]
+            else:
+                card_obj.image_url = card["card_faces"][0]["image_uris"]["normal"]
+            card_obj.price = card["prices"]["usd"]
+            card_obj.type_line = card["type_line"]
+            if "mana_cost" in card:
+                card_obj.mana_cost = card["mana_cost"]
+            else:
+                card_obj.mana_cost = card["card_faces"][0]["mana_cost"]
+            card_obj.latest_card_data = card
+            ckd_price, ckd_image = get_ckd_price_and_img(card["name"])
+            card_obj.price_ckd = ckd_price
+            card_obj.image_url_ckd = ckd_image
+            card_obj.save()
+            return card_obj
+    else:
+        print("scryfall had an issue code:", response.status_code)
+        print("error:", response.json())
+    return None
+
+
 def scryfall_search(query):
     query = " ".join(query.split()).lower()
     last_24_hours = timezone.now() - timedelta(hours=24)
@@ -38,46 +83,4 @@ def scryfall_search(query):
     if existing_card_qs.exists():
         return existing_card_qs.first()
     else:
-        print("doing query")
-        url = "https://api.scryfall.com/cards/search"
-        params = {
-            "q": query,
-            "unique": "prints",
-            "order": "name",
-            "dir": "asc",
-        }
-
-        response = requests.get(url, params=params)
-
-        if response.status_code == 200:
-            data = response.json()
-
-            if data["object"] == "error":
-                print("Scryfall had an error", data)
-                return None
-
-            if data["total_cards"] > 0:
-                card = data["data"][0]
-
-                card_obj, _ = MtgCard.objects.get_or_create(search_text=query)
-                card_obj.name = card["name"]
-                if "image_uris" in card:
-                    card_obj.image_url = card["image_uris"]["normal"]
-                else:
-                    card_obj.image_url = card["card_faces"][0]["image_uris"]["normal"]
-                card_obj.price = card["prices"]["usd"]
-                card_obj.type_line = card["type_line"]
-                if "mana_cost" in card:
-                    card_obj.mana_cost = card["mana_cost"]
-                else:
-                    card_obj.mana_cost = card["card_faces"][0]["mana_cost"]
-                card_obj.latest_card_data = card
-                ckd_price, ckd_image = get_ckd_price_and_img(card["name"])
-                card_obj.price_ckd = ckd_price
-                card_obj.image_url_ckd = ckd_image
-                card_obj.save()
-                return card_obj
-        else:
-            print("scryfall had an issue code:", response.status_code)
-            print("error:", response.json())
-        return None
+        return update_query_from_scryfall(query)
